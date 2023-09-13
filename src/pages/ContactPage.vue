b1b0cf3614347838666e7ccc2717b8f738501075
GLCVLL <g.ioele96@outlook.it>
2023-09-13 19:35:52 +0200

    fixed error in contact page

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,26 +1,65 @@
 <script>
+import axios from 'axios';
+const endpoint = 'http://127.0.0.1:8000/api/contact-message';
+const emptyForm = { email: '', subject: '', message: '', subscription: false };
 export default {
     name: 'ContactPage',
+    data: () => ({ form: emptyForm }),
+
+    methods: {
+        sendForm() {
+            axios.post(endpoint, this.form)
+                .then(() => { this.form = emptyForm })
+                .catch(err => { console.error(err) })
+                .then(() => { })
+        }
+    }
 }
 </script>
 
 <template>
-    <form>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
-        </div>
-        <div class="mb-3 form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <div class="container">
+        <h2 class="text-center my-5">Contact Us</h2>
+
+        <!-- form -->
+        <form @submit.prevent="sendForm">
+            <div class="row">
+                <div class="col-md-6">
+                    <!-- email -->
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email address<sub class="text-danger">*</sub></label>
+                        <input type="email" class="form-control" id="email" placeholder="example@hello.com"
+                            v-model.trim="form.email">
+                        <small class="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+
+                    <!-- Object -->
+                    <div class="mb-3">
+                        <label for="subject" class="form-label">Email Object<sub class="text-danger">*</sub></label>
+                        <input type="text" class="form-control" id="subject" v-model.trim="form.subject">
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <!-- message -->
+                    <div class="mb-3">
+                        <label class="form-label" for="message">Message<sub class="text-danger">*</sub></label>
+                        <textarea class="form-control" id="message" rows="5" v-model.trim="form.message"></textarea>
+                    </div>
+
+                    <!-- newsletter -->
+                    <div class="mb-3 form-check">
+                        <input class="form-check-input" type="checkbox" id="subscription" v-model="form.subscription">
+                        <label class="form-check-label" for="subscription">Subscribe to our Newsletter</label>
+                    </div>
+
+                    <div class="mb-3 d-flex justify-content-end">
+                        <button class="btn btn-primary">Send</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
 </template>
 
 <style></style>
